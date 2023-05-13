b3d74c7372c15ad1f4323e9469d3c544f2e21db9
Peter Bjorklund <piot@hotmail.com>
2023-05-13 10:08:36 +0200

    chore: initial commit

---
+++ src/lib/daemon.h
@@ -0,0 +1,18 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Peter Bjorklund. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+#ifndef CONCLAVE_DAEMON_DAEMON_H
+#define CONCLAVE_DAEMON_DAEMON_H
+
+#include <conclave-server/server.h>
+#include <udp-server/udp_server.h>
+
+typedef struct ClvDaemon {
+    ClvServer server;
+    UdpServerSocket socket;
+} ClvDaemon;
+
+int clvDaemonInit(ClvDaemon* self);
+
+#endif

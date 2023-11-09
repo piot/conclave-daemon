a73507d91830ea3dc3c89f1af38c0db98f9c824c
Peter Bjorklund <piot@hotmail.com>
2023-11-09 21:24:44 +0100

    fix: use guise-client-udp

--- src/lib/main.c
+++ src/lib/main.c
@@ -2,34 +2,35 @@
  *  Copyright (c) Peter Bjorklund. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-#include <clog/clog.h>
-#include <clog/console.h>
 #include "daemon.h"
 #include "version.h"
+#include <clog/console.h>
 #include <flood/in_stream.h>
 #include <flood/out_stream.h>
+#include <guise-client-udp/client.h>
+#include <guise-client-udp/read_secret.h>
 #include <imprint/default_setup.h>
 
-#if !TORNADO_OS_WINDOWS
+#if !defined TORNADO_OS_WINDOWS
+#include <time.h>
 #include <unistd.h>
 #endif
 
 clog_config g_clog;
 
-typedef struct UdpServerSocketSendToAddress
-{
-    struct sockaddr_in *sockAddrIn;
-    UdpServerSocket *serverSocket;
+typedef struct UdpServerSocketSendToAddress {
+    struct sockaddr_in* sockAddrIn;
+    UdpServerSocket* serverSocket;
 } UdpServerSocketSendToAddress;
 
-static int sendToAddress(void *self_, const ClvAddress *address, const uint8_t *buf, size_t count)
+static int sendToAddress(void* self_, const uint8_t* buf, size_t count)
 {
-    UdpServerSocketSendToAddress *self = (UdpServerSocketSendToAddress *)self_;
+    UdpServerSocketSendToAddress* self = (UdpServerSocketSendToAddress*)self_;
 
     return udpServerSend(self->serverSocket, buf, count, self->sockAddrIn);
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     (void)argc;
     (void)argv;
@@ -38,28 +39,42 @@ int main(int argc, char *argv[])
 
     CLOG_OUTPUT("conclave daemon v%s starting up", CONCLAVE_DAEMON_VERSION)
 
-    ClvDaemon daemon;
+    ImprintDefaultSetup memory;
+    imprintDefaultSetupInit(&memory, 16 * 1024 * 1024);
 
+    ClvDaemon daemon;
     int err = clvDaemonInit(&daemon);
-    if (err < 0)
-    {
+    if (err < 0) {
         return err;
     }
+
+    GuiseClientUdp guiseClient;
+
+    Clog guiseClientLog;
+    guiseClientLog.config = &g_clog;
+    guiseClientLog.constantPrefix = "GuiseClient";
+
+    const char* guiseHost = "127.0.0.1";
+    uint16_t guisePort = 27004;
+
+    GuiseClientUdpSecret secret;
+    guiseClientUdpReadSecret(&secret);
+
+    guiseClientUdpInit(&guiseClient, &memory.tagAllocator.info, guiseHost, guisePort, &secret);
+    // guiseClientUdpReInit(&guiseClient, &guiseTransport, secret.userId, secret.passwordHash);
+    GuiseClientState reportedState = GuiseClientStateIdle;
 
     UdpServerSocketSendToAddress socketSendToAddress;
     socketSendToAddress.serverSocket = &daemon.socket;
 
-    ClvServerSendDatagram sendDatagram;
-    sendDatagram.send = sendToAddress;
-    sendDatagram.self = &socketSendToAddress;
+    DatagramTransportOut transportOut;
+    transportOut.self = &socketSendToAddress;
+    transportOut.send = sendToAddress;
 
     ClvResponse response;
-    response.sendDatagram = sendDatagram;
+    response.transportOut = &transportOut;
 
     ClvServer server;
-
-    ImprintDefaultSetup memory;
-    imprintDefaultSetupInit(&memory, 16 * 1024 * 1024);
 
     // TODO:    ConclaveSerializeVersion applicationVersion = {0x10, 0x20, 0x30};
 
@@ -67,14 +82,11 @@ int main(int argc, char *argv[])
     serverLog.constantPrefix = "ClvServer";
     serverLog.config = &g_clog;
 
-    clvServerInit(&server, &memory.tagAllocator.info, serverLog);
-
 #define UDP_MAX_SIZE (1200)
 
     uint8_t buf[UDP_MAX_SIZE];
     size_t size;
     struct sockaddr_in address;
-    int errorCode;
 
 #define UDP_REPLY_MAX_SIZE (UDP_MAX_SIZE)
 
@@ -83,17 +95,40 @@ int main(int argc, char *argv[])
     fldOutStreamInit(&outStream, reply, UDP_REPLY_MAX_SIZE);
 
     CLOG_OUTPUT("ready for incoming UDP packets")
+    bool hasCreatedConclaveDaemon = false;
+    while (true) {
 
-    while (true)
-    {
-        size = UDP_MAX_SIZE;
-        errorCode = udpServerReceive(&daemon.socket, buf, &size, &address);
-        if (errorCode < 0)
-        {
-            CLOG_WARN("problem with receive %d", errorCode);
+        struct timespec ts;
+
+        ts.tv_sec = 0;
+        ts.tv_nsec = 16 * 1000000;
+        nanosleep(&ts, &ts);
+
+        if (!hasCreatedConclaveDaemon) {
+            MonotonicTimeMs now = monotonicTimeMsNow();
+            guiseClientUdpUpdate(&guiseClient, now);
         }
-        else
-        {
+
+        if (reportedState != guiseClient.guiseClient.state) {
+            reportedState = guiseClient.guiseClient.state;
+            if (reportedState == GuiseClientStateLoggedIn && !hasCreatedConclaveDaemon) {
+                clvServerInit(&server, guiseClient.transport,
+                    guiseClient.guiseClient.mainUserSessionId, &memory.tagAllocator.info,
+                    serverLog);
+                CLOG_C_INFO(&server.log, "server authenticated")
+                hasCreatedConclaveDaemon = true;
+                //guiseClientUdpDestroy(&guiseClient);
+            }
+        }
+        if (!hasCreatedConclaveDaemon) {
+            continue;
+        }
+
+        size = UDP_MAX_SIZE;
+        ssize_t errorCode = udpServerReceive(&daemon.socket, buf, size, &address);
+        if (errorCode < 0) {
+            CLOG_WARN("problem with receive %zd", errorCode)
+        } else {
             socketSendToAddress.sockAddrIn = &address;
 
             fldOutStreamRewind(&outStream);
@@ -101,14 +136,13 @@ int main(int argc, char *argv[])
             nimbleSerializeDebugHex("received", buf, size);
 #endif
             errorCode = clvServerFeed(&server, &address, buf, size, &response);
-            if (errorCode < 0)
-            {
-                CLOG_WARN("clvServerFeed: error %d", errorCode);
+            if (errorCode < 0) {
+                CLOG_WARN("clvServerFeed: error %zd", errorCode)
             }
         }
     }
 
-    imprintDefaultSetupDestroy(&memory);
+    // imprintDefaultSetupDestroy(&memory);
 
-    return 0;
+    // return 0;
 }
